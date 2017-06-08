cda07df2eb0684c44a14592620c49947dfcdcaa1
s0dz <garrett.mctear@gmail.com>
2017-06-07 22:51:13 -0600

    Cleaning up

---
+++ PackageInstallerUnitTests/DependencyValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using PackageInstallerUtility;
+
+namespace PackageInstallerUnitTests
+{
+    [TestFixture]
+    public class DependencyValidatorTests
+    {
+        private DependencyValidator _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new DependencyValidator();
+        }
+
+        [Test]
+        public void ValidateTestInput1()
+        {
+            var result = _validator.Validate(validInput1);
+
+            Assert.AreEqual(result, validOutput1);
+        }
+
+        [Test]
+        public void ValidateTestInput2()
+        {
+            var result = _validator.Validate(validInput2);
+
+            Assert.AreEqual(result, validOutput2);
+        }
+
+        [Test]
+        public void InvalidateTestInput()
+        {
+            var result = _validator.Validate(invalidInput);
+
+            Assert.AreEqual(result, invalidOutput);
+        }
+
+        //private string validInput1 = "[\"KittenService: CamelCaser\", \"CamelCaser: \"]";
+        private string[] validInput1 = { "KittenService: CamelCaser", "CamelCaser: " };
+        private string validOutput1 = "CamelCaser, KittenService";
+
+        private string[] validInput2 = { "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " };
+        private string validOutput2 = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
+
+        private string[] invalidInput = { "KittenService: ","Leetmeme: Cyberportal", "Cyberportal: Ice","CamelCaser: KittenService","Fraudstream: ","Ice: Leetmeme" };
+        private string invalidOutput = "Invalid Input: Dependencies contain a cycle";
+    }
+}
--- PackageInstallerUtility/DependencyValidator.cs
+++ PackageInstallerUtility/DependencyValidator.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace PackageInstallerExercise
+namespace PackageInstallerUtility
 {
     public class DependencyValidator
     {
