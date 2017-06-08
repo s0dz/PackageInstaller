be4f1ece3400d5d8d5b7a48d9948b9566bfb67e9
s0dz <garrett.mctear@gmail.com>
2017-06-07 21:15:13 -0600

    Stubbed out tests and validator

---
+++ PackageInstallerExercise/DependencyValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PackageInstallerExercise
+{
+    public class DependencyValidator
+    {
+        public string Validate(List<string> packageDependencies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
